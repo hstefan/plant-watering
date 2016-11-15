6fcc2ef65ed6d735257ed8c7e055e63a7a80e4f1
Hugo Puhlmann <hugopuhlmann@gmail.com>
2016-11-15 15:57:02 -0200

    Adding simple two relay test routine.

---
+++ arduino/TwoRelayTest/TwoRelayTest.ino
@@ -0,0 +1,34 @@
+const int sol0Pin = 13;
+const int sol1Pin = 12;
+
+void setup() {
+  pinMode(sol0Pin, OUTPUT);
+  pinMode(sol1Pin, OUTPUT);
+}
+
+void delayPreempt(unsigned long ms) {
+  // for now this is just a wrapper for "delay"
+  delay(ms);
+}
+
+void loop() {
+  // wait 30 seconds before doing anything
+  delayPreempt(30000L);
+
+  // enables first solenoid
+  digitalWrite(sol0Pin, LOW);
+  // waits for sixty seconds
+  delayPreempt(60000L);
+  // disables first solenoid
+  digitalWrite(sol0Pin, HIGH);
+
+  // waits one second
+  delayPreempt(1000L);
+
+  // enables second solenoid
+  digitalWrite(sol1Pin, LOW);
+  // waits 60 seconds
+  delayPreempt(60000L);
+  // disables second solenoid
+  digitalWrite(sol1Pin, HIGH);
+}
