a1749c7d82d7f771bb1fec206f6785696c392d19
Hugo Puhlmann <hugopuhlmann@gmail.com>
2016-11-25 18:42:57 -0200

    Cleanly exiting the sequential relay test in case we get an Exception.

--- python/rpi_relay_test/sequential.py
+++ python/rpi_relay_test/sequential.py
@@ -47,4 +47,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as ex:
+        print('Exception caught, quitting.')
+        print(ex)
+        reset()
